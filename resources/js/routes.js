394d2a4766ac97a38f6a693b10e169038735770d
codeplaners <codeplaners@gmail.com>
2021-05-29 19:24:25 +0530

    Laravel project

---
+++ resources/js/routes.js
@@ -0,0 +1,22 @@
+
+import AllPost from './components/AllPost.vue';
+import AddPost from './components/AddPost.vue';
+import EditPost from './components/EditPost.vue';
+ 
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: AllPost
+    },
+    {
+        name: 'add',
+        path: '/add',
+        component: AddPost
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditPost
+    }
+];
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+ 
+Route::get('posts', 'PostController@index');
+Route::group(['prefix' => 'post'], function () {
+    Route::post('add', 'PostController@add');
+    Route::get('edit/{id}', 'PostController@edit');
+    Route::post('update/{id}', 'PostController@update');
+    Route::delete('delete/{id}', 'PostController@delete');
+});
